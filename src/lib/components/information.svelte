d73f24092c7cfd51b4d15c2ac040a8c3464be35b
divaisworqueen <divaisworqueen@gmail.com>
2025-06-16 04:09:37 +0000

    modify information board to add scroll area

--- src/lib/components/information.svelte
+++ src/lib/components/information.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card"
-    import {Button} from "$lib/components/ui/button"
-    import {Badge} from "$lib/components/ui/badge"
+    import { Button } from "$lib/components/ui/button"
+    import { Badge } from "$lib/components/ui/badge"
+    import { ScrollArea } from "$lib/components/ui/scroll-area"
     import NextIcon from "@lucide/svelte/icons/chevron-right";
 </script>
 
-<Card.Root class="w-full max-w-sm">
-    <Card.Header> 
+<Card.Root class="w-full max-w-sm p-4">
+    <Card.Header class="pt-4"> 
         <Card.Title>Informasi Terkini</Card.Title>
         <Card.Description>Pengumuman terbaru di BPS Kabupaten Rokan Hilir</Card.Description>
         <Card.Action>
             <Button>Lihat semua</Button>
         </Card.Action>
     </Card.Header>
-    <Card.Content>
+    <ScrollArea class="h-72 w-full rounded-md border p-4">
         <!-- add perulangan disini -->
         <!-- contoh -->
         <Card.Root>
@@ -79,5 +80,5 @@
                 </Card.Action>
             </Card.Header>
         </Card.Root>
-    </Card.Content>
+    </ScrollArea>
 </Card.Root>
