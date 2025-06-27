913746f753f048e03829a57dceb23a4f8048d672
dipzzyyy <99947557+dipzzyyy@users.noreply.github.com>
2025-06-27 14:40:20 +0700

    nyamnyamnyamnyam (it will error on supabase thingy - deleted some crucial connection info)

--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -1,3 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+const something = ""
+const somethingElse = ""
 
-export const supabase = createClient('https://gtwqpxkuxhjarjobtdeo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0d3FweGt1eGhqYXJqb2J0ZGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NTEzOTcsImV4cCI6MjA2NjMyNzM5N30.D-31izSTqYxdssKNdPIDgDhQdHlF9IzdEmWxcxAQI1U')
+export const supabase = createClient(something, somethingElse)
